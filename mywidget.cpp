2acf99e82013326544bca2087bafc81a2277f35c
BilibiliW <1878126372@qq.com>
2021-04-01 21:37:18 +0800

    qt commit test

--- mywidget.cpp
+++ mywidget.cpp
@@ -1,12 +1,14 @@
 #include "mywidget.h"
 #include <QPushButton>
+#include <QDebug>
 myWidget::myWidget(QWidget *parent)
     : QWidget(parent)
 {
     QPushButton * btn=new QPushButton;
     //btn->show();
+    qDebug("sssss");
     btn->setParent(this);
-
+//
 }
 
 myWidget::~myWidget()
